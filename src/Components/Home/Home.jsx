be3d627dbfa5d84cf56ded30974a74621a335bd6
Develekko <Housamsoma99@gmail.com>
2023-09-30 01:47:36 +0300

    update

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,15 +1,44 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import React from "react";
+import React from 'react';
 import "./Home.css";
+import BoxShot from '../../assets/Box_Shot_04.png'
+import DotBig from '../../assets/dot-big.png'
+import DotSmall from '../../assets/dot-small.png'
+import WebsiteIntro from '../../assets/01_WEBSITE_INTRO.mp4'
 
 
 export default function Home() {
 
-
-
-  return (
-   <>
-    <h1 className="text-large font-bold">Hi , Welcome </h1>
-   </>
-  );
+  return <>
+    <div className="container">
+      <div className="row justify-content-center">
+        <div className="col-lg-8 col-12">
+          <div className=" text-center mb-5 mb-md-7 text_animation show">
+            <h1><p><strong>@</strong>media</p>
+            </h1>
+            <div>A smart way to earn rewards for every advertisement you see while watching TV, your favourite film, TV show or while playing a videogame.<br />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="row justify-content-center">
+        <div className="w-full">
+          <div className="top__video text-center">
+            <img loading="lazy" className="top__video__smalldot" src={DotSmall} alt="dot" />
+            <img loading="lazy" className="top__video__bigdot" src={DotBig} alt="dot" />
+            <video width="100%" height="auto" muted autoPlay playsInline className="loaded" >
+              <source src={WebsiteIntro} type="video/mp4" />
+            </video>
+          </div>
+        </div>
+      </div>
+      <div className="row justify-content-center">
+        <div className="col-lg-12 col-md-10 col-8">
+          <div className="top__box-image text-center">
+            <img loading="lazy" src={BoxShot} alt="MediaBox" />
+          </div>
+        </div>
+      </div>
+    </div>
+  </>
 }
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -2,6 +2,7 @@
 import React from 'react'
 import './Navbar.css'
 import { Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, Input, DropdownItem, DropdownTrigger, Dropdown, DropdownMenu, Avatar, NavbarMenu, NavbarMenuItem, useDisclosure, Modal, NavbarMenuToggle, Progress, useSwitch, VisuallyHidden } from "@nextui-org/react";
+import { motion } from 'framer-motion';
 
 
 export default function NavbarComponent() {
@@ -9,49 +10,52 @@ export default function NavbarComponent() {
   const [isMenuOpen, setIsMenuOpen] = React.useState(false);
 
   const menuItems = [
-    {name:"World map",url:"#"},
-    {name:"what is @ahmed",url:"#"},
-    {name:"how it works",url:"#"},
-    {name:"customizable",url:"#"},
-    {name:"business information",url:"#"},
-    {name:"video",url:"#"},
+    { name: "World map", url: "#" },
+    { name: "what is @ahmed", url: "#" },
+    { name: "how it works", url: "#" },
+    { name: "customizable", url: "#" },
+    { name: "business information", url: "#" },
+    { name: "video", url: "#" },
   ];
 
 
 
   return <>
     <Navbar
+      maxWidth="xl"
+      height="5rem"
+      isBlurred={false}
       isMenuOpen={isMenuOpen}
       onMenuOpenChange={setIsMenuOpen}
       className={`bg-transparent backdrop-filter-none backdrop-blur-none transition-background duration-1000`}
     >
       <NavbarContent>
-       
         <NavbarBrand>
-          <p className="font-bold text-inherit">ACME</p>
+          <p className="font-bold text-large">@ahmed</p>
         </NavbarBrand>
       </NavbarContent>
-      <NavbarContent className="hidden sm:flex gap-4" justify="center">
+      <NavbarContent className="hidden sm:flex gap-8" justify="end">
         <NavbarItem><Link color="foreground" href="https://www.lyconet.com/ie/mediabox">World map</Link></NavbarItem>
-        <NavbarItem><Link href="#whatis@ahmed" aria-current="page">what is @ahmed</Link></NavbarItem>
+        <NavbarItem><Link color="foreground" href="#whatis@ahmed">what is @ahmed</Link></NavbarItem>
         <NavbarItem><Link color="foreground" href="#howitworks">how it works</Link></NavbarItem>
         <NavbarItem><Link color="foreground" href="#customizable">customizable</Link></NavbarItem>
         <NavbarItem><Link color="foreground" href="#business">business information</Link></NavbarItem>
         <NavbarItem><Link color="foreground" href="#video">video</Link></NavbarItem>
       </NavbarContent>
 
-      <NavbarContent justify="end">
-        <NavbarItem className="hidden lg:flex">
-          <Link href="#">Login</Link>
-        </NavbarItem>
-        <NavbarMenuToggle
-          aria-label={isMenuOpen ? "Close menu" : "Open menu"}
-          className="sm:hidden"
-        />
+      <NavbarContent className="sm:hidden" justify="end">
+        <NavbarMenuToggle aria-label={isMenuOpen ? "Close menu" : "Open menu"} />
       </NavbarContent>
-   
 
-      <NavbarMenu>
+
+      <NavbarMenu
+        className="w-5/6 top-0 z-50"
+        initial={{ x: '-100%' }}
+        animate={{ x: '0%' }}
+        exit={{ x: '-100%' }}
+        transition={{ duration: 0.3 }}
+      >
+      <p className="text-center my-3">Menu</p>
         {menuItems.map((item, index) => (
           <NavbarMenuItem key={`${item.name}-${index}`}>
             <Link
