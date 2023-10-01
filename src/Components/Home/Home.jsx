fe4fefb6ac2a0a889fbf77aba05e2c90290a9481
Housam Ramadan <Housamsoma99@gmail.com>
2023-10-01 11:01:49 +0300

    update

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -22,12 +22,12 @@ export default function Home() {
     <motion.section
       style={{ opacity: HeroOpacitySection }} id='hero' className='flex fixed flex-col justify-start w-full text-center h-screen gap-5 -z-10 pt-5 min-h-screen '>
 
-    <AnimationOnScroll animateIn="animate__fadeIn" animateOut='animate__fadeOut'>
-      <div className="text-center flex flex-col items-center gap-7 justify-center md:w-2/3 mx-auto">
-        <p className='text-5xl'><strong className="text-red-700">@</strong>ahmed</p>
-        <p className='text-large text-zinc-400'>A smart way to earn rewards for every advertisement you see while watching TV, your favourite film, TV show or while playing a videogame.</p>
-      </div>
-      </AnimationOnScroll> 
+      <AnimationOnScroll animateIn="animate__fadeIn" animateOut='animate__fadeOut'>
+        <div className="text-center flex flex-col items-center gap-7 justify-center md:w-2/3 mx-auto">
+          <p className='text-5xl'><strong className="text-red-700">@</strong>ahmed</p>
+          <p className='text-large text-zinc-400'>A smart way to earn rewards for every advertisement you see while watching TV, your favourite film, TV show or while playing a videogame.</p>
+        </div>
+      </AnimationOnScroll>
       <div className="relative text-center md:w-2/3 mx-auto md:px-10 w-5/6 mt-5">
         <img loading="lazy" className="absolute top-0 right-14 translate-x-full -translate-y-28" src={DotSmall} alt="dot" />
         <img loading="lazy" className="absolute top-0 right-0 translate-x-full translate-y-10" src={DotBig} alt="dot" />
@@ -48,12 +48,12 @@ export default function Home() {
     <motion.section id='whatis' className='bg-black py-36'>
       <div className='container m-auto px-10'>
         <div className='grid grid-cols-12'>
-            <div className='col-span-12 sm:col-span-6 md:col-span-6'>
+          <div className='col-span-12 sm:col-span-6 md:col-span-6'>
             <AnimationOnScroll animateIn="animate__fadeInUp" animateOut='animate__fadeOutUp'>
-                <p className='text-sm uppercase font-normal text-red-700 -translate-y-10'>What is @ahmed</p>
-                <h3 className='text-5xl text-white -translate-y-6'>All in <span className='text-red-700'>one box</span></h3>
-              </AnimationOnScroll>
-            </div>
+              <p className='text-sm uppercase font-normal text-red-700 -translate-y-10'>What is @ahmed</p>
+              <h3 className='text-5xl text-white -translate-y-6'>All in <span className='text-red-700'>one box</span></h3>
+            </AnimationOnScroll>
+          </div>
           <div className='col-span-12 sm:col-span-6 md:col-span-6'>
             <Parallax>
               <p>@media box is the first multi-media TV box designed for home entertainment where you can watch TV, films or TV shows from your favourite streaming providers and get paid. Thanks to our myWorld partnership you will get a commission in form of Cashback and additional benefits for every ad you see.</p>
@@ -62,14 +62,14 @@ export default function Home() {
 
         </div>
         <Parallax rotateX={[100, -90]} >
-        <Image loading="lazy" src={MediaBox} alt="MediaBox" className=' mx-auto w-4/5' />
+          <Image loading="lazy" src={MediaBox} alt="MediaBox" className='mx-auto w-4/5' classNames={{wrapper:"mx-auto"}}/>
         </Parallax>
         <div className='col-span-12'>
-            <AnimationOnScroll animateIn="animate__fadeInUp" animateOut='animate__fadeOutUp'>
-             <h2 className='text-white'>About myWorld International</h2>
-              <p className='text-zinc-400'>The London-based myWorld International group operates the worldwide myWorld Benefit Program, which offers a wealth of attractive benefits for shoppers, retailers and service providers alike. Shoppers can enjoy Shopping Points and Cashback with every purchase from myWorld partners regardless of whether they shop online or in-store. In turn, partners benefit from efficient customer loyalty programmes, which help them increase their visibility and sales. More than 15 million customers and 150,000 partners in 55 markets already use the myWorld Benefit Program. More information on myworld.com.</p>
-            </AnimationOnScroll>
-          </div>
+          <AnimationOnScroll animateIn="animate__fadeInUp" animateOut='animate__fadeOutUp'>
+            <h2 className='text-white'>About myWorld International</h2>
+            <p className='text-zinc-400'>The London-based myWorld International group operates the worldwide myWorld Benefit Program, which offers a wealth of attractive benefits for shoppers, retailers and service providers alike. Shoppers can enjoy Shopping Points and Cashback with every purchase from myWorld partners regardless of whether they shop online or in-store. In turn, partners benefit from efficient customer loyalty programmes, which help them increase their visibility and sales. More than 15 million customers and 150,000 partners in 55 markets already use the myWorld Benefit Program. More information on myworld.com.</p>
+          </AnimationOnScroll>
+        </div>
 
       </div>
     </motion.section>
