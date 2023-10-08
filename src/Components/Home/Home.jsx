527cc0ddd509b6b20927626cc68ac1a29b88f1ab
Housam Ramadan <Housamsoma99@gmail.com>
2023-10-08 10:56:09 +0300

    tbc

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -6,20 +6,19 @@ import DotBig from '../../assets/images/dot-big.png'
 import DotSmall from '../../assets/images/dot-small.png'
 import MediaBox from '../../assets/images/MediaBox_Rot_01_00015.png'
 import HeroIntroVideo from '../../assets/videos/01_WEBSITE_INTRO.mp4'
-import { Image, Tabs, Tab } from '@nextui-org/react';
+import { Image } from '@nextui-org/react';
 import { motion, useTransform, useScroll } from "framer-motion";
 import { Parallax } from "react-scroll-parallax";
 import { AnimationOnScroll } from 'react-animation-on-scroll';
-import { HowToWorkItems } from './HowToWork.jsx';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { EffectFade, Keyboard, Pagination } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/bundle'
+import HowItWorks from '../HowItWorks/HowItWorks.jsx';
 
 export default function Home() {
   const { scrollYProgress } = useScroll();
   const HeroOpacitySection = useTransform(scrollYProgress, [0, 1], [1, 0]);
-  const [selected, setSelected] = React.useState("Plug and play");
   const [videoIntroLoaded, setVideoIntroLoaded] = React.useState(false);
 
   return <>
@@ -82,58 +81,7 @@ export default function Home() {
     </motion.section>
 
 
-    <section id="howitworks" style={{ backgroundColor: '#F4F4F4' }} className='py-36 text-black'>
-      <div className='container m-auto  px-0 sm:px-10 text-center'>
-        <p className="uppercase text-red-600 text-sm tracking-wider mb-3">How it works</p>
-        <h2 className='text-2xl sm:text-5xl font-medium'>Connect, watch & get paid</h2>
-
-        <div className="flex w-full flex-col mt-5">
-          <Tabs
-            aria-label="howitworksTabs"
-            variant='underlined'
-            color='default'
-            className='mx-auto text-black'
-            classNames={{
-              base: 'text-black',
-              cursor: "w-full bg-[#000]",
-              tabContent: "group-data-[selected=true]:text-[#000]"
-            }}
-            selectedKey={selected}
-            onClick={(e) => { setSelected((e.target.outerText)) }}
-            onSelectionChange={setSelected}
-          >
-            {HowToWorkItems.map((item) => {
-              return (
-                <Tab key={item.label} title={item.label}>
-                  <div className='grid grid-cols-12 py-10 px-5 relative howitworks_section'>
-                    <div className="col-span-12 sm:col-span-6 md:col-span-6 text-left flex flex-col justify-center">
-                      <AnimationOnScroll duration="0.7" animateIn="animate__fadeInLeft" animateOut='animate__fadeOutLeft'>
-                        <h4 className='text-2xl font-medium text-black'>{item.heading}</h4>
-                        <p className='md:w-2/3 text-base text-zinc-400 my-5'>{item.desc}</p>
-                      </AnimationOnScroll>
-                    </div>
-                    <div className="col-span-12 sm:col-span-6 md:col-span-6">
-                      <AnimationOnScroll duration="0.7" animateIn="animate__fadeInRight" animateOut='animate__fadeOutRight'>
-                        <video width="100%" height="auto" muted loop autoPlay playsInline
-                          poster="anyimage.jpg" className="bg-black">
-                          <source data-src={item.url} type="video/mp4" src={item.url} />
-                        </video>
-                        {item.label !== 'Plug and play' && <div className="howitworks__video__image absolute w-full">
-                          <div className="relative howitworks__video__image">
-                            <img loading="lazy" className="mx-auto" width={280} src="https://atmedia.digital/wp-content/themes/mediaworld/assets/images/Box_Shot_03-small.png" alt="@ ahmed box - TV Entertainment and Advertising" />
-                          </div>
-                        </div>}
-                      </AnimationOnScroll>
-                    </div>
-                    <div className="absolute w-full top-0 h-full"></div>
-                  </div>
-                </Tab>
-              );
-            })}
-          </Tabs>
-        </div>
-      </div>
-    </section>
+    <HowItWorks/>
 
 
     <motion.section id="buy" style={{ backgroundColor: '#F4F4F4' }}>
@@ -169,7 +117,7 @@ export default function Home() {
       </div>
     </motion.section>
 
-
+            
     <motion.section id="customizable" style={{ backgroundColor: '#fff' }}>
       <div className='container m-auto py-20 px-5 md:px-32 text-center'>
         <div className="grid grid-cols-12">
